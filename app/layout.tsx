2a0a6d9074ae3f76f918008e022c8d4b84af18d1
kidwiththestickpublishingllc-ship-it <kidwiththestickpublishingllc@gmail.com>
2026-04-18 20:37:17 -0400

    feat: add Google AdSense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Playfair_Display, Inter } from 'next/font/google'
+import Script from 'next/script'
 import PageChatWidget from "./components/PageChatWidget";
 import WelcomeTour from "./components/WelcomeTour";
 
@@ -20,6 +21,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${playfair.variable} ${inter.variable}`}>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6696631641081046"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="font-inter bg-[#FAFAF8] text-[#111111]">
         {children}
         <PageChatWidget />
